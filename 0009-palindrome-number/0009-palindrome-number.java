9d899eb3f241692da73fa20a2e0760e05e2b72fe
Jarganaya <90854362+JavierGavra@users.noreply.github.com>
2026-09-20 11:13:11 +0700

    Time: 8 ms (7.25%), Space: 46.3 MB (6.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        StringBuilder number = new StringBuilder();
+        number.append(x);
 
-        if (x < 10) return true;
-
-        char[] number = (x + "").toCharArray();
-
-        final int halfLength = number.length / 2;
-        for (int i = 0; i < halfLength; i++) {
-            if (number[i] != number[number.length - i - 1]){
-                return false;
-            }
-        }
-
-        return true;
+        return number.toString().contentEquals(number.reverse());
     }
 }
